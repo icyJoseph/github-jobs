d62cc9922c2259866925cc4ae29cc894c35d7b2e
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-03-15 22:21:58 +0100

    chore: Initial commit

---
+++ src/ui/App.tsx
@@ -0,0 +1,13 @@
+import { Fragment, useState } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <header>
+        <h1>GitHub Jobs</h1>
+      </header>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./src")
+    }
+  }
+});
