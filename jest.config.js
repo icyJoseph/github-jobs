29ce9621e9de347031033fc70c0092922e02dff9
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-03-19 16:08:58 +0100

    chore: Add css module mappers

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  // moduleDirectories: ["node_modules", "./src"],
   moduleNameMapper: {
+    ".(css|less|sass|scss)$": "jest-transform-css",
     "~/(.*)": "<rootDir>/src/$1"
   }
 };
