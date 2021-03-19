c16f366663bd9005cb8c533789dcb23df88908aa
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-03-19 16:07:50 +0100

    chore: Move handler mocks to root

--- setupTests.ts
+++ setupTests.ts
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/extend-expect";
 import { setupServer } from "msw/node";
 
-import { handlers } from "./src/mocks/handlers";
+import { handlers } from "./mocks/handlers";
 
 const server = setupServer(...handlers);
 
